b197b5e7254ba30821713bfd8ffbefd3aad80c05
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-09-04 07:20:28 +0000

    Create motors.lua

---
+++ motors.lua
@@ -0,0 +1,15 @@
+return {
+	['Right Shoulder'] = CF.n(1.5,.5,0),
+	['Left Shoulder']  = CF.n(-1.5,.5,0),
+	['Right Hip']      = CF.n(.5,-1,0),
+	['Left Hip']       = CF.n(-.5,-1,0),
+	['Neck']           = CF.n(0,1.5,0),
+	['RootJoint']      = CF.n(0,0,0)
+}, {
+	['Right Shoulder'] = CF.n(0,.5,0),
+	['Left Shoulder']  = CF.n(0,.5,0),
+	['Right Hip']      = CF.n(0,1,0),
+	['Left Hip']       = CF.n(0,1,0),
+	['Neck']           = CF.n(0,0,0),
+	['RootJoint']      = CF.n(0,0,0)
+}
