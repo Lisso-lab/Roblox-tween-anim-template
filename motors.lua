3e76dde7581363712f1c0e16f5dfc512831391e9
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-09-04 08:41:13 +0000

    Update motors.lua

--- motors.lua
+++ motors.lua
@@ -1,27 +1,28 @@
+--!strict
 local C0 = {
-	['Right Shoulder'] = CF.n(1.5,.5,0),
-	['Left Shoulder']  = CF.n(-1.5,.5,0),
-	['Right Hip']      = CF.n(.5,-1,0),
-	['Left Hip']       = CF.n(-.5,-1,0),
-	['Neck']           = CF.n(0,1.5,0),
-	['RootJoint']      = CF.n(0,0,0)
+	["Right Shoulder"] = CF.n(1.5,.5,0),
+	["Left Shoulder"]  = CF.n(-1.5,.5,0),
+	["Right Hip"]      = CF.n(.5,-1,0),
+	["Left Hip"]       = CF.n(-.5,-1,0),
+	["Neck"]           = CF.n(0,1.5,0),
+	["RootJoint"]      = CF.n(0,0,0)
 }
 local C1 = {
-	['Right Shoulder'] = CF.n(0,.5,0),
-	['Left Shoulder']  = CF.n(0,.5,0),
-	['Right Hip']      = CF.n(0,1,0),
-	['Left Hip']       = CF.n(0,1,0),
-	['Neck']           = CF.n(0,0,0),
-	['RootJoint']      = CF.n(0,0,0)
+	["Right Shoulder"] = CF.n(0,.5,0),
+	["Left Shoulder"]  = CF.n(0,.5,0),
+	["Right Hip"]      = CF.n(0,1,0),
+	["Left Hip"]       = CF.n(0,1,0),
+	["Neck"]           = CF.n(0,0,0),
+	["RootJoint"]      = CF.n(0,0,0)
 }
 
 local shortned_names = {
-	['Right Shoulder'] = "RA"
-	['Left Shoulder']  = "LA",
-	['Right Hip']      = "RL",
-	['Left Hip']       = "LL",
-	['Neck']           = "HED",
-	['RootJoint']      = "TOR"
+	["RA"] = "Right Shoulder",
+	["LA]" = "Left Shoulder",
+	["RL"] = "Right Hip",
+	["LL"] = "Left Hip",
+	["HED"] = "Neck",
+	["TOR"] = "RootJoint"
 }
 
 return {C0=C0, C1=C1, names=shortned_names}
