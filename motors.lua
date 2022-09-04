42a1a69315f6a6b7edaeb59e8c9968503c739fd2
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-09-04 07:21:52 +0000

    Update motors.lua

--- motors.lua
+++ motors.lua
@@ -1,11 +1,11 @@
-return {
+return {C0 = {
 	['Right Shoulder'] = CF.n(1.5,.5,0),
 	['Left Shoulder']  = CF.n(-1.5,.5,0),
 	['Right Hip']      = CF.n(.5,-1,0),
 	['Left Hip']       = CF.n(-.5,-1,0),
 	['Neck']           = CF.n(0,1.5,0),
 	['RootJoint']      = CF.n(0,0,0)
-}, {
+}, C1 = {
 	['Right Shoulder'] = CF.n(0,.5,0),
 	['Left Shoulder']  = CF.n(0,.5,0),
 	['Right Hip']      = CF.n(0,1,0),
